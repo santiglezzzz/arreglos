5d2199a043d4c72615ac276bc549f2a1b8e98952
santiglezzzz <45fn9rh66f@privaterelay.appleid.com>
2026-09-21 07:42:45 -0600

    Add files via upload

---
+++ practica12/js/index.js
@@ -0,0 +1,35 @@
+let lista = ["Lavar uniforme", "Buscar tachones", 
+    "Ganar el partido","Otro tarea","Estudiar para el examen",
+    "Hacer la tarea de programación","Lavar calcetines"];
+
+console.log(lista);
+
+console.log(lista[0]);
+console.log(lista[1]);
+console.log(lista[2]);
+
+console.log(lista[3]);
+
+console.log(lista[-10]);
+
+let arregloNumeros = [1,2,3,4,5,6,7,8,9];
+console.log(arregloNumeros);
+console.log(arregloNumeros[8]);
+console.log(arregloNumeros[4]);
+console.log(lista[3]);
+console.log(lista[5]);
+
+let superHeroes = ["Superman", "Batman", 
+    "Mujer Maravilla", "Flash", "Green Lantern"];
+
+
+superHeroes[0] = "Thor";
+superHeroes[1] = "Iron Man";
+superHeroes[2] = "Capitán América";
+superHeroes[3] = "Hulk";
+superHeroes[4] = "Black Panther";
+superHeroes[5] = "Doctor Strange";
+
+console.log(superHeroes);
+
+
